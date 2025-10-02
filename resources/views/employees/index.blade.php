a8b8b0846755dd32dd591e392159af51306d04f8
Septareno Nugroho Aji <renoaji25sep@gmail.com>
2025-10-02 14:53:10 +0700

    penambahan Master layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,24 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    </head>
-    <body>
-        <div class="container mt-5">
-        <h1 class="mb-4">Daftar Pegawai</h1>
-        <table border="1" cellpadding="5" cellspacing="0">
-            <thead>
+@extends('master')
+@section('title', 'Daftar Pegawai')
+@section('content')
+<div class="container mt-5">
+    <h1 class="mb-4">Daftar Pegawai</h1>
+    <table border="1" cellpadding="5" cellspacing="0">
+        <thead>
             <tr>
-                <th>Nama Lengkap</th>
-                <th>Email</th>
-                <th>Nomor Telepon</th>
-                <th>Tanggal Lahir</th>
-                <th>Alamat</th>
-                <th>Tanggal Masuk</th>
-                <th>Status</th>
-                <th>Aksi</th>
+            <th>Nama Lengkap</th>
+            <th>Email</th>
+            <th>Nomor Telepon</th>
+            <th>Tanggal Lahir</th>
+            <th>Alamat</th>
+            <th>Tanggal Masuk</th>
+            <th>Status</th>
+            <th>Aksi</th>
             </tr>
-            </thead>
-            <tbody>
+        </thead>
+        <tbody>
             @foreach($employees as $employee)
             <tr>
                 <td>{{ $employee->nama_lengkap }}</td>
@@ -29,19 +27,17 @@
                 <td>{{ $employee->tanggal_masuk }}</td>
                 <td>{{ $employee->status }}</td>
                 <td>
-                <a href="{{ route('employees.show', $employee->id) }}">Detail</a> |
-                <a href="{{ route('employees.edit', $employee->id) }}">Edit</a> |
-                <form action="{{ route('employees.destroy', $employee->id) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" onclick="return confirm('Yakin ingin menghapus?')">Delete</button>
-                </form>
+                    <a href="{{ route('employees.show', $employee->id) }}">Detail</a> |
+                    <a href="{{ route('employees.edit', $employee->id) }}">Edit</a> |
+                    <form action="{{ route('employees.destroy', $employee->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" onclick="return confirm('Yakin ingin menghapus?')">Delete</button>
+                    </form>
                 </td>
             </tr>
             @endforeach
-            </tbody>
-        </table>
-        </div>
-        <a href="employees/create">Tambah data</a>
-    </body>
-</html>
+        </tbody>
+    </table>
+</div>
+@endsection
